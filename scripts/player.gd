fb508b508393c0d736e233168e2c201f54e04e3b
Luca Engelhard <me@lucaengelhard.com>
2024-07-18 11:44:11 +0200

    cone

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,12 @@
+class_name Player
+
 extends CharacterBody2D
 
 enum TEAM {CT, T}
 enum MOVEMODE {Mouse, Random}
 
 # draw player
+@export_group("Player")
 @export var team: TEAM = TEAM.CT
 @export var size := 7
 @onready var collider := $Collider
@@ -15,7 +18,23 @@ enum MOVEMODE {Mouse, Random}
 @export var movemode := MOVEMODE.Mouse # Change default later
 @onready var target := get_new_target()
 @onready var map := $"../Map"
-	
+
+@export_group("Vision")
+@export_range(0, 360) var Angle := 90
+@export var Distance := 500
+@export var RayNumber := 100
+@export var RayNode : RayCast2D
+@onready var rays: Array[RayCast2D] = []
+
+func _ready():
+	create_cone()
+
+func _process(_delta):
+	return
+	for ray in rays:
+		if ray.is_colliding():
+			print("colliding")
+
 func _draw():
 	var color = Color.GREEN
 	if team == TEAM.CT:
@@ -24,7 +43,8 @@ func _draw():
 		color = Color.RED
 	collider.shape.radius = size
 	draw_circle(Vector2(0,0), size, color)
-	
+
+#navigation
 func _physics_process(delta):
 	nav_to_point(delta)
 	
@@ -69,6 +89,27 @@ func get_new_random_target() -> Vector2:
 			currentMapPos.y - mapSize.y / 2, currentMapPos.y + mapSize.y /2)
 			)
 
-
 func _on_navigation_agent_2d_target_reached():
 	target = get_new_target()
+
+#viewcones
+func create_cone():
+	for i in range(RayNumber):
+		add_child(RayCast2D.new())
+	
+	var children := get_children()
+	
+	var newRays: Array[RayCast2D] = []
+	
+	for child in children:	
+		if child is RayCast2D:
+			newRays.append(child)
+	
+	for i in newRays.size():
+		var ray := newRays[i]
+		var rayAngle := (float(Angle) / float(RayNumber)) * float(i)
+		ray.exclude_parent = true
+		ray.target_position = Vector2(0, Distance)
+		ray.rotation = deg_to_rad(rayAngle)
+			
+	rays = newRays
